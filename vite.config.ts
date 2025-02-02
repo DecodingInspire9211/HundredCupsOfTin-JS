8cbadea39a60da577a17cf2fa517497b43206e34
Kenneth-William Beier <beierkw9211@gmail.com>
2025-02-02 18:33:15 +0100

    changed deploy.yaml

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+    root: '.', // Set the root directory to the project root
     base: '/HundredCupsOfTin-JS/',
     build: {
-        assetsDir: '.',
+        outDir: 'dist', // Output directory
+        assetsDir: '.', // Place assets directly in the output directory
+        rollupOptions: {
+            input: {
+                main: 'index.html', // Entry point for the build
+            },
+        },
     },
 })
